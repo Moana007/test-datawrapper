65a9f11f232ada297a200668a8732836129273e5
iMac Studio 24 <gestion@iMac-de-iMac-2.local>
2015-08-20 12:57:06 +0200

    first commit

---
+++ plugin.php
@@ -0,0 +1,41 @@
+<?php
+
+class DatawrapperPlugin_VisualizationTreemap extends DatawrapperPlugin {
+
+    public function init() {
+
+        $visMeta = array(
+            /*
+             * The unique visualization id (not to be confused with the plugin id)
+             */
+            "id" => "visualization-treemap",
+
+            /*
+             * The title displayed in the editor UI. Wrap in __() to make it
+             * localizable.
+             */
+            "title" => "Treemap",
+            "extends": "raphael-chart",
+
+            /*
+             * Optional: libraries that are used by the visualization.
+             */
+            "libraries" => array(array(
+                "local" => "vendor/d3.treemap.js"
+            )),
+
+            "order" => 70,
+            "dimensions" => 1,
+            "color-by" => "hierarchy",
+            /*
+             * The config options that are displayed to the user in the right sidebar
+             * in the chart editor. In this case three options are defined for assigning
+             * the columns to each of the axes.
+             */
+            "options" => array()     
+        );
+
+        DatawrapperVisualization::register($this, $visMeta);
+    }
+
+}
